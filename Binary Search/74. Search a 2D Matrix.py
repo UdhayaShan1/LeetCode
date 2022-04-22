4a2a1304b8461c4eb1a1a1a97d48dd03462141e5
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-04-22 23:20:42 +0800

    Create 74. Search a 2D Matrix.py

---
+++ Binary Search/74. Search a 2D Matrix.py
@@ -0,0 +1,24 @@
+from bisect import bisect_left 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        list1 = []
+        for i in matrix:
+            list1 += i
+
+        def BinarySearch(a, x):
+            i = bisect_left(a, x)
+            if i:
+                return (i-1)
+            else:
+                return -1
+
+        res = BinarySearch(list1,target+1)
+
+        if res == -1:
+            return(False)
+        else:
+            if list1[res] != target:
+                return(False)
+            else:
+                return(True)
+        
