635b594dd0514c3237e2a8229877e260e7dc98f1
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-05-01 23:42:54 +0800

    Create 139. Word Break.py

---
+++ Dynamic Programming/139. Word Break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = {}
+        dp[len(s)] = True
+        for i in range(len(s)-1,-1,-1):
+            flag = 0
+            dp[i] = False
+            for j in wordDict:
+                length = len(j)
+                if s[i:i+length] == j and len(s[i:i+length]) == length:
+                    if dp[i+length] == True:
+                        dp[i] = True
+                        flag+=1
+                        break
+
+        return(dp[0])
