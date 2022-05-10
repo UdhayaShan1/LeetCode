e410b52485ccfcc66fb803f3fb6c05082309e586
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-05-10 15:02:10 +0800

    Create 35. Search Insert Position.py

---
+++ Binary Search/35. Search Insert Position.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        b1 = False
+        left = 0
+        flag = 0
+        while b1==False:
+            if len(nums) == 0:
+                break
+            index = len(nums)//2
+            if nums[index] < target:
+                nums = nums[index+1:]
+                left += index+1
+            elif nums[index] > target:
+                nums = nums[:index]
+            else:
+                nums = nums[index:]
+                left += index
+                flag+=1
+                break
+
+        return(left)
