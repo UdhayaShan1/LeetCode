f6706287ce78ab191d7b32274f852f5c9b12484f
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2022-07-24 21:17:57 +0800

    Create 695. Max Area of Island.py

---
+++ Graph, Matrix/695. Max Area of Island.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        dp_spread = {}
+        dp2 = {}
+
+        def dfs(i,j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+                return
+            if (i,j) in dp2:
+                return
+            if grid[i][j] == 0:
+                return
+            dp_spread[(i,j)] = 1
+            dp2[(i,j)] = 1
+            y1 = dfs(i+1,j)
+            y2 = dfs(i-1,j)
+            y3 = dfs(i,j-1)
+            y4 = dfs(i,j+1)
+
+        max1 = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    dp_spread = {}
+                    dfs(i,j)
+                    max1 = max(max1,len(dp_spread.keys()))
+        return max1
